3115dff264f146d9d50b23f9c1a67ab9e5680260
Hannah Angela Tablizo <hannah angela@your-mom>
2023-11-09 20:15:22 +0800

    Initial commit

--- src/ex06_gra_tablizohl/FireType.java
+++ src/ex06_gra_tablizohl/FireType.java
@@ -3,7 +3,7 @@ package ex06_gra_tablizohl;
 
 public class FireType extends SimpleMonster {
     
-    public FireType(String n, String t, String s, String w, int m, int base) {
+    public FireType(String n, int m, int base) {
         super("Charmander", "fire", "grass", "water", m, base);
     }
     
